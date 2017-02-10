aad772079a3bd2362cb85b2c57f24edeaf5a7b57
Jeff Choi <hyuch93@gmail.com>
2017-02-10 15:12:15 -0500

    Added stats and formatting

--- src/MainPlayer.java
+++ src/MainPlayer.java
@@ -1,11 +1,21 @@
 public class MainPlayer extends Player {
+    private int numWins;
+    private int numLosses;
+    private int numDraws;
+
     public MainPlayer() {
         this.name = "";
+        this.numWins = 0;
+        this.numLosses = 0;
+        this.numDraws = 0;
     }
 
     public MainPlayer(String name, char value) {
         this.name = name;
         this.value = value;
+        this.numWins = 0;
+        this.numLosses = 0;
+        this.numDraws = 0;
     }
 
     public String getName() {
@@ -14,6 +24,30 @@ public class MainPlayer extends Player {
     
     public char getValue() {
         return this.value;
+    }
+
+    public void addWins() {
+        this.numWins += 1;
+    }
+
+    public void addLosses() {
+        this.numLosses += 1;
+    }
+
+    public void addDraws() {
+        this.numDraws += 1;
+    }
+
+    public int getWins() {
+        return this.numWins;
+    }
+
+    public int getLosses() {
+        return this.numLosses;
+    }
+
+    public int getDraws() {
+        return this.numDraws;
     }
 
     public String printValue() {
@@ -40,4 +74,15 @@ public class MainPlayer extends Player {
     public char generateRoshambo() {
         return this.value;
     }
+
+    /*
+    specs wanted a generateRoshambo that took in an input and put that out
+    im doing things a little differently so doing things differnetly???
+
+    public char generateRoshambo(String input) {
+        return input.toLowerCase().charAt(0);
+    }
+
+
+    */
 }
--- src/RoshamboApp.java
+++ src/RoshamboApp.java
@@ -33,7 +33,6 @@ public class RoshamboApp{
             holder = validate.getString(sc).toLowerCase().charAt(0);
         }
         return holder;
-
     }
 
     public static String playGame(MainPlayer player, Player opponent) {
@@ -42,59 +41,66 @@ public class RoshamboApp{
 
         if (player.getValue() == 'r' || player.getValue() == 'R') {
             if (opponent.getValue() == 'r' || opponent.getValue() == 'R') {
+                player.addDraws();
                 return "Draw!";
             }
             else if (opponent.getValue() == 'p' || opponent.getValue() == 'P') {
+                player.addLosses();
                 return opponent.getName() + " wins!";
             }
             else {
+                player.addWins();
                 return player.getName() + " wins!";
             } 
         }
         else if (player.getValue() == 'p' || player.getValue() == 'P') {
             if (opponent.getValue() == 'r' || opponent.getValue() == 'R') {
+                player.addWins();
                 return player.getName() + " wins!";
             }
             else if (opponent.getValue() == 'p' || opponent.getValue() == 'P') {
+                player.addDraws();
                 return "Draw!";
             }
             else {
+                player.addLosses();
                 return opponent.getName() + " wins!";
             } 
 
         }
         else {
             if (opponent.getValue() == 'r' || opponent.getValue() == 'R') {
+                player.addLosses();
                 return opponent.getName() + " wins!";
             }
             else if (opponent.getValue() == 'p' || opponent.getValue() == 'P') {
+                player.addWins();
                 return player.getName() + " wins!";
             }
             else {
+                player.addDraws();
                 return "Draw!";
             } 
-
         }
     }    
-
 
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
         Validator validate = new Validator();
+        MainPlayer player = new MainPlayer();
 
         System.out.println("Welcome to Rock Paper Scissors!");
         System.out.println("");
         
         System.out.print("Enter your name: ");
-        String userName = validate.getString(sc);
+        player.setName(validate.getString(sc));
 
         System.out.print("Would you like to play against the Jets(j) or Sharks(s): ");
         char userOption = getUserOption(sc, validate);
 
         while (true) {
             System.out.print("Rock, paper, or scissors (r/p/s)? ");
-            char handChoice = getHandChoice(sc, validate);
-            MainPlayer player = new MainPlayer(userName, handChoice);
+            player.setValue(getHandChoice(sc, validate));
             String result = "";
 
             // change below to methods
@@ -114,14 +120,12 @@ public class RoshamboApp{
             char toContinue = getContinue(sc, validate);
 
             if (toContinue == 'n') {
-                System.out.println("Thanks for playing!");
+                System.out.println("Thanks for playing, here are your stats!");
+                System.out.println("Wins: " + player.getWins());
+                System.out.println("Losses: " + player.getLosses());
+                System.out.println("Draws: " + player.getDraws());
                 break;
             }
-
         }
-
-
-
     }
-
 }
